1d2ea37e0d1a9d5db9cbd5a8bef8dd4d450b019c
Mohit <mohitjangid38437@gmail.com>
2024-02-06 01:48:22 +0530

    update music player for mobile device

--- Spotify Clone/script.js
+++ Spotify Clone/script.js
@@ -86,9 +86,10 @@ let playingSong = {
 
 const songsCardWrapper = document.querySelector(".songs-card-wrapper");
 const musicPlayerEl = document.querySelector("#music-player");
-const currentSongImgEl = musicPlayerEl.querySelector("#current-song-img");
-const currentSongNameEl = musicPlayerEl.querySelector("#cur-song-name");
-const currentSongArtistNameEl = musicPlayerEl.querySelector("#cur-song-artist");
+const showCurrentPlayingSongInPlayerEl = musicPlayerEl.querySelector(
+  "#playing-current-song"
+);
+
 const playPauseBtn = musicPlayerEl.querySelector("#play-paused-btn");
 const volumeBtn = musicPlayerEl.lastElementChild;
 const seekBarInMusisPlayer = musicPlayerEl.querySelector(".song-seekbar");
@@ -169,10 +170,39 @@ function musicPlayer(songCard) {
   playSong(songAudio);
 }
 
-function showCurrentSongInPlayer(img, name, artist) {
-  currentSongImgEl.innerHTML = img;
-  currentSongNameEl.innerText = name;
-  currentSongArtistNameEl.innerText = artist;
+function showCurrentSongInPlayer(img, songName, artist) {
+  const imgWrapper = document.createElement("div");
+  imgWrapper.classList.add("song-pic");
+  imgWrapper.innerHTML = img;
+
+  const songDetailsWrapper = document.createElement("div");
+  songDetailsWrapper.classList.add("current-song-details");
+  songDetailsWrapper.innerHTML = `
+  <h5 class="song-name" >${songName}</h5>
+  <p class="song-artist" >${artist}</p>
+  `;
+
+  const favIconWrapper = document.createElement("div");
+  favIconWrapper.className = "favorite-icon";
+  favIconWrapper.innerHTML = `
+  <svg
+  data-encore-id="icon"
+  role="img"
+  aria-hidden="true"
+  viewBox="0 0 16 16"
+  class="transition"
+>
+  <path
+    d="M1.69 2A4.582 4.582 0 0 1 8 2.023 4.583 4.583 0 0 1 11.88.817h.002a4.618 4.618 0 0 1 3.782 3.65v.003a4.543 4.543 0 0 1-1.011 3.84L9.35 14.629a1.765 1.765 0 0 1-2.093.464 1.762 1.762 0 0 1-.605-.463L1.348 8.309A4.582 4.582 0 0 1 1.689 2zm3.158.252A3.082 3.082 0 0 0 2.49 7.337l.005.005L7.8 13.664a.264.264 0 0 0 .311.069.262.262 0 0 0 .09-.069l5.312-6.33a3.043 3.043 0 0 0 .68-2.573 3.118 3.118 0 0 0-2.551-2.463 3.079 3.079 0 0 0-2.612.816l-.007.007a1.501 1.501 0 0 1-2.045 0l-.009-.008a3.082 3.082 0 0 0-2.121-.861z"
+  ></path>
+</svg>
+  `;
+  // favorite icon eventlistender
+  favIconWrapper.addEventListener("click", addToFavorite);
+  showCurrentPlayingSongInPlayerEl.innerHTML = "";
+  showCurrentPlayingSongInPlayerEl.appendChild(imgWrapper);
+  showCurrentPlayingSongInPlayerEl.appendChild(songDetailsWrapper);
+  showCurrentPlayingSongInPlayerEl.appendChild(favIconWrapper);
 }
 
 function musicPlayerControls() {
@@ -286,6 +316,19 @@ function removeRepeatingSongs() {
   }
 }
 
+function addToFavorite() {
+  let fillSvg = ` <svg
+  data-encore-id="icon"
+  role="img"
+  aria-hidden="true"
+  viewBox="0 0 16 16"
+>
+  <path
+    d="M15.724 4.22A4.313 4.313 0 0 0 12.192.814a4.269 4.269 0 0 0-3.622 1.13.837.837 0 0 1-1.14 0 4.272 4.272 0 0 0-6.21 5.855l5.916 7.05a1.128 1.128 0 0 0 1.727 0l5.916-7.05a4.228 4.228 0 0 0 .945-3.577z"
+  ></path>
+</svg> `;
+}
+
 function playSong(song) {
   playPauseBtn.classList.add("active");
   playPauseSong(playPauseBtn);
@@ -388,28 +431,43 @@ function toggleVolumeBtn(e) {
   volumeBarContainer.classList.toggle("show");
 }
 
-let timeoutId = null;
 function increseDecreseVolume(e) {
   let showVoumeEl = e.currentTarget.nextElementSibling;
   showVoumeEl.innerHTML = e.currentTarget.value;
   playingSong.currentSong.volume = e.currentTarget.value / 100;
 }
 
+function expandMusicPlayer(e) {
+  if (window.innerWidth < 992) {
+    const closeExpandViewIcon = musicPlayerEl.querySelector(
+      "#expand-player-view"
+    );
+    closeExpandViewIcon.addEventListener("click", (e) => {
+      musicPlayerEl.classList.remove("expand");
+      setTimeout(() => {
+        musicPlayerEl.addEventListener("click", expandMusicPlayer);
+      }, 10);
+    });
+
+    this.classList.add("expand");
+    if (this.classList.contains("expand")) {
+      this.removeEventListener("click", expandMusicPlayer);
+    }
+  }
+}
+
+// event listeners
 seekBarInMusisPlayer.addEventListener("input", (e) => {
   let position = (e.currentTarget.value / 100) * 100;
 
-  let changeDuration = (playingSong.currentSong.currentTime =
-    (Math.floor(position) / 100) * playingSong.currentSong.duration);
+  playingSong.currentSong.currentTime =
+    (Math.floor(position) / 100) * playingSong.currentSong.duration;
 });
 
 volumeBtn.addEventListener("click", toggleVolumeBtn);
-
-const isMobile = window.innerWidth < 991 ? true : false;
-
 volumeBtn.querySelector("input").addEventListener("change", (e) => {
   e.currentTarget.parentElement.classList.remove("show");
 });
-
 volumeBtn
   .querySelector("input")
   .addEventListener("input", increseDecreseVolume);
@@ -439,3 +497,6 @@ songsCardWrapper.innerHTML = "";
 songData.forEach((song) => {
   appendSongs(song);
 });
+
+// mobile view player
+musicPlayerEl.addEventListener("click", expandMusicPlayer);
